faf385203da8c947e5f1eec5c6f2eb43cef76975
xspynks <xspynks@gmail.com>
2022-01-24 23:27:43 -0300

    Mudanças no Menu.

--- menu.js
+++ menu.js
@@ -1,7 +1,21 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu() {
+function toggleMenu(event) {
+    if (event.type === 'touchstart')
+        event.preventDefault()
     const navigation = document.getElementById('navigation');
-    navigation.classList.toggle('active');
+    nav.classList.toggle('active');
+    const active = navigation.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active)
+
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+
+    else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+
 }
+
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
