da6479ee794f5eebfb327735660d2ac35a4e81e1
Rafael de Paula <xspynks@gmail.com>
2022-01-24 23:17:34 -0300

    Primeiro commit do projeto.

---
+++ menu.js
@@ -0,0 +1,7 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+    const navigation = document.getElementById('navigation');
+    navigation.classList.toggle('active');
+}
+btnMobile.addEventListener('click', toggleMenu);
